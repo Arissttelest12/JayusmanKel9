de7754b3d52b614d500572104a6396272f65d1a0
rlzckyardnsyh <rlzckyardnsyh@gmail.com>
2026-05-25 12:29:09 +0700

    rizky crud models cabang kategori barang stok transaksi

---
+++ app/Models/StokKeluar.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StokKeluar extends Model
+{
+    protected $table = 'stok_keluar';
+    protected $primaryKey = 'id_stok_keluar';
+    protected $fillable = ['id_cabang', 'id_barang', 'id_user', 'jumlah', 'tanggal_keluar', 'alasan'];
+    protected $casts = [
+        'tanggal_keluar' => 'date',
+    ];
+
+    public function cabang()
+    {
+        return $this->belongsTo(Cabang::class, 'id_cabang', 'id_cabang');
+    }
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'id_barang', 'id_barang');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+}
